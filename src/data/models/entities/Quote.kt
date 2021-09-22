a9d752b8db0347b513939457fd1bf2f14bf867b4
Syed Hussain Mehdi <shmehdi01@gmail.com>
2021-09-22 14:23:22 +0530

    REFACTOR: made authorId nullable

--- src/data/models/entities/Quote.kt
+++ src/data/models/entities/Quote.kt
@@ -7,7 +7,7 @@ data class Quote(
     val id: Int,
     val quote: String,
     val author: String,
-    val authorId: String,
+    val authorId: String?,
     val userId: Int,
     val isActive: Boolean
 )
